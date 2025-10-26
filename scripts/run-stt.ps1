c878d7bf0e33ae7c823a615557de67eb344e8396
mmprotest <simon.villani@gmail.com>
2025-10-26 17:26:58 +1100

    Reset

--- scripts/run-stt.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-. .\.venv-stt\Scripts\Activate.ps1
-$env:STT_PORT = $env:STT_PORT -as [string]
-if (-not $env:STT_PORT) { $env:STT_PORT = "5007" }
-python -m stt_service.main
